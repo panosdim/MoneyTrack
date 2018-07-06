550b81583c7826429b93a840c40b9594d9f699be
Panagiotis Dimopoulos <padi@intracom-telecom.com>
2018-07-06 10:51:04 +0300

    Use username instead of email. UI fixes.

--- app/src/main/java/com/panosdim/moneytrack/WebServiceHandler.java
+++ app/src/main/java/com/panosdim/moneytrack/WebServiceHandler.java
@@ -5,25 +5,18 @@ import android.util.Log;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class WebServiceHandler {
-    public String performPostCall(String requestURL,
-                                   JSONObject jsonParam) {
+    private static final String TAG = "MT_WEB_SERVICE";
 
-        final String TAG = "MT_APP";
+    public String performPostCall(String requestURL,
+                                  JSONObject jsonParam) {
         URL url;
         StringBuilder response = new StringBuilder();
         try {
@@ -35,27 +28,23 @@ public class WebServiceHandler {
             conn.setRequestMethod("POST");
             conn.setDoInput(true);
             conn.setDoOutput(true);
-            conn.setRequestProperty("Content-Type","application/json");
+            conn.setRequestProperty("Content-Type", "application/json");
 
-            Log.i(TAG, jsonParam.toString());
-
-            DataOutputStream printout = new DataOutputStream(conn.getOutputStream ());
+            DataOutputStream printout = new DataOutputStream(conn.getOutputStream());
             printout.writeBytes(jsonParam.toString());
-            printout.flush ();
-            printout.close ();
+            printout.flush();
+            printout.close();
 
 
-
-            int responseCode=conn.getResponseCode();
+            int responseCode = conn.getResponseCode();
 
             if (responseCode == HttpsURLConnection.HTTP_OK) {
                 String line;
-                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                while ((line=br.readLine()) != null) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = br.readLine()) != null) {
                     response.append(line);
                 }
-            }
-            else {
+            } else {
                 response = new StringBuilder();
 
             }
@@ -64,22 +53,5 @@ public class WebServiceHandler {
         }
 
         return response.toString();
-    }
-
-    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
-        for(Map.Entry<String, String> entry : params.entrySet()){
-            if (first)
-                first = false;
-            else
-                result.append("&");
-
-            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-        }
-
-        return result.toString();
     }
 }
