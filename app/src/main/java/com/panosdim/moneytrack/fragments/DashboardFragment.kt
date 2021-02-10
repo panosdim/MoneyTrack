c0ed04242d905ecdb6c25a732798ad61f662c179
Panagiotis Dimopoulos <padi@intracom-telecom.com>
2021-02-10 16:52:50 +0200

    Update Dashboard to select month and year.

--- app/src/main/java/com/panosdim/moneytrack/fragments/DashboardFragment.kt
+++ app/src/main/java/com/panosdim/moneytrack/fragments/DashboardFragment.kt
@@ -7,6 +7,7 @@ import android.text.style.RelativeSizeSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -26,26 +27,29 @@ import com.panosdim.moneytrack.utils.moneyFormat
 import com.panosdim.moneytrack.viewmodel.ExpensesViewModel
 import com.panosdim.moneytrack.viewmodel.IncomeViewModel
 import kotlinx.android.synthetic.main.fragment_dashboard.*
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.time.Month
 
 class DashboardFragment : Fragment(), OnChartValueSelectedListener {
     private val expensesViewModel: ExpensesViewModel by viewModels(ownerProducer = { this })
     private val incomeViewModel: IncomeViewModel by viewModels(ownerProducer = { this })
     private lateinit var monthExpensesPerCategories: List<PieEntry>
     private val today = LocalDate.now()
-    private val startOfMonth = today.withDayOfMonth(1)
-    private val endOfMonth = today.withDayOfMonth(today.lengthOfMonth())
-    private val startOfYear = today.withDayOfMonth(1).withMonth(1)
-    private val endOfYear = today.withMonth(12).withDayOfMonth(31)
+    private var selectedMonth = today.month
+    private var selectedYear = today.year
+    private var startOfMonth = today.withDayOfMonth(1)
+    private var endOfMonth = today.withDayOfMonth(today.lengthOfMonth())
+    private var startOfYear = today.withDayOfMonth(1).withMonth(1)
+    private var endOfYear = today.withMonth(12).withDayOfMonth(31)
     private var incomeList: List<Income> = mutableListOf()
     private var expensesList: List<Expense> = mutableListOf()
     private var categoriesList: List<Category> = mutableListOf()
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
@@ -67,40 +71,76 @@ class DashboardFragment : Fragment(), OnChartValueSelectedListener {
             initializeChart()
         }
 
+        val monthAdapter = ArrayAdapter(
+            requireContext(),
+            R.layout.list_item,
+            Month.values()
+        )
+        root.overviewMonth.setAdapter(monthAdapter)
+        // Workaround as there is a bug in AutoCompleteView filter setting
+        root.overviewMonth.setText(selectedMonth.name, false)
+
+        root.overviewMonth.setOnItemClickListener { parent, _, position, _ ->
+            selectedMonth = parent.getItemAtPosition(position) as Month
+            val s = LocalDate.of(selectedYear, selectedMonth, 1)
+            startOfMonth = s.withDayOfMonth(1)
+            endOfMonth = s.withDayOfMonth(s.lengthOfMonth())
+            startOfYear = s.withDayOfMonth(1).withMonth(1)
+            endOfYear = s.withMonth(12).withDayOfMonth(31)
+            initializeSavings()
+            initializeChart()
+        }
+
+        val currentYear = today.year
+        val years = IntArray(4) { currentYear - it }
+        val yearAdapter = ArrayAdapter(
+            requireContext(),
+            R.layout.list_item,
+            years.toTypedArray()
+        )
+        root.overviewYear.setAdapter(yearAdapter)
+        // Workaround as there is a bug in AutoCompleteView filter setting
+        root.overviewYear.setText(selectedYear.toString(), false)
+
+        root.overviewYear.setOnItemClickListener { parent, _, position, _ ->
+            selectedYear = parent.getItemAtPosition(position) as Int
+            val s = LocalDate.of(selectedYear, selectedMonth, 1)
+            startOfYear = s.withDayOfMonth(1).withMonth(1)
+            endOfYear = s.withMonth(12).withDayOfMonth(31)
+            startOfMonth = s.withDayOfMonth(1)
+            endOfMonth = s.withDayOfMonth(s.lengthOfMonth())
+            initializeSavings()
+            initializeChart()
+        }
+
         return root
     }
 
     private fun initializeSavings() {
-        val monthFormatter = DateTimeFormatter.ofPattern("MMMM yyyy")
-        val yearFormatter = DateTimeFormatter.ofPattern("yyyy")
-
-        txtMonthSavingsTitle.text = monthFormatter.format(today)
-        txtYearSavingsTitle.text = getString(R.string.savings_title, yearFormatter.format(today))
-
         val totalMonthIncome = incomeList.filter {
             val date = LocalDate.parse(it.date)
             (date.isAfter(startOfMonth) || date.isEqual(startOfMonth)) &&
                     (date.isBefore(endOfMonth) || date.isEqual(endOfMonth))
         }
-                .map { it.amount }
-                .sum()
+            .map { it.amount }
+            .sum()
 
         val totalMonthExpenses = expensesList.filter {
             val date = LocalDate.parse(it.date)
             (date.isAfter(startOfMonth) || date.isEqual(startOfMonth)) &&
                     (date.isBefore(endOfMonth) || date.isEqual(endOfMonth))
         }
-                .map { it.amount }
-                .sum()
+            .map { it.amount }
+            .sum()
 
         val totalMonthSavings = totalMonthIncome - totalMonthExpenses
 
         txtMonthSavings.text = moneyFormat(totalMonthSavings)
         txtMonthSavings.setTextColor(
-                ContextCompat.getColor(
-                        requireContext(),
-                        calculateColor(totalMonthSavings)
-                )
+            ContextCompat.getColor(
+                requireContext(),
+                calculateColor(totalMonthSavings)
+            )
         )
 
         val totalYearIncome = incomeList.filter {
@@ -108,25 +148,25 @@ class DashboardFragment : Fragment(), OnChartValueSelectedListener {
             (date.isAfter(startOfYear) || date.isEqual(startOfYear)) &&
                     (date.isBefore(endOfYear) || date.isEqual(endOfYear))
         }
-                .map { it.amount }
-                .sum()
+            .map { it.amount }
+            .sum()
 
         val totalYearExpenses = expensesList.filter {
             val date = LocalDate.parse(it.date)
             (date.isAfter(startOfYear) || date.isEqual(startOfYear)) &&
                     (date.isBefore(endOfYear) || date.isEqual(endOfYear))
         }
-                .map { it.amount }
-                .sum()
+            .map { it.amount }
+            .sum()
 
         val totalYearSavings = totalYearIncome - totalYearExpenses
 
         txtYearSavings.text = moneyFormat(totalYearSavings)
         txtYearSavings.setTextColor(
-                ContextCompat.getColor(
-                        requireContext(),
-                        calculateColor(totalYearSavings)
-                )
+            ContextCompat.getColor(
+                requireContext(),
+                calculateColor(totalYearSavings)
+            )
         )
     }
 
@@ -140,15 +180,15 @@ class DashboardFragment : Fragment(), OnChartValueSelectedListener {
             (date.isAfter(startOfMonth) || date.isEqual(startOfMonth)) &&
                     (date.isBefore(endOfMonth) || date.isEqual(endOfMonth))
         }
-                .groupBy { it.category }
-                .map { (k, v) ->
-                    PieEntry(
-                            v.map { it.amount }.sum(),
-                            categoriesList.find { it.id == k }!!.category
-                    )
+            .groupBy { it.category }
+            .map { (k, v) ->
+                PieEntry(
+                    v.map { it.amount }.sum(),
+                    categoriesList.find { it.id == k }!!.category
+                )
 
-                }
-                .sortedBy { it.value }
+            }
+            .sortedBy { it.value }
 
         val set = PieDataSet(monthExpensesPerCategories, "Expenses Per Categories")
 
@@ -170,6 +210,9 @@ class DashboardFragment : Fragment(), OnChartValueSelectedListener {
         chart.setOnChartValueSelectedListener(this)
         data.setValueFormatter(PercentFormatter(chart))
         chart.data = data
+        chart.onTouchListener.setLastHighlighted(null)
+        chart.highlightValues(null)
+        chart.centerText = ""
         chart.invalidate()
     }
 
@@ -183,21 +226,21 @@ class DashboardFragment : Fragment(), OnChartValueSelectedListener {
             val centerString = SpannableString(catEntry.label + "\n" + moneyFormat(e.y))
             centerString.setSpan(RelativeSizeSpan(2f), 0, catEntry.label.length, 0)
             centerString.setSpan(
-                    RelativeSizeSpan(3f),
-                    catEntry.label.length + 1,
-                    centerString.length,
-                    0
+                RelativeSizeSpan(3f),
+                catEntry.label.length + 1,
+                centerString.length,
+                0
             )
             centerString.setSpan(
-                    ForegroundColorSpan(
-                            ContextCompat.getColor(
-                                    requireContext(),
-                                    android.R.color.holo_red_dark
-                            )
-                    ),
-                    catEntry.label.length + 1,
-                    centerString.length,
-                    0
+                ForegroundColorSpan(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        android.R.color.holo_red_dark
+                    )
+                ),
+                catEntry.label.length + 1,
+                centerString.length,
+                0
             )
             chart.centerText = centerString
         }
